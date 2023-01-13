f7fb00c26b212d07019c6ee84a7fdffcbeac83c5
Andreye <drontop1999@mail>
2023-01-13 22:50:30 +0300

    добавили измененияи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+
+
+Console.WriteLine("Введите два целых числа: a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите два целых числа: b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a / b == b) 
+{
+    Console.WriteLine($"Первое число {a} является квадратом второго {b}");
+}
+else
+{
+    Console.WriteLine($"Первое число {a} не является квадратом второго {b}");
+}
+
+
+
